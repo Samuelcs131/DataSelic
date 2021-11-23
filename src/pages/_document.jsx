62ff4f7d7c0af0f44d06255a4970cd1b06840d4a
Samuel Claudino <samuelcs131@gmail.com>
2021-11-23 18:28:53 -0300

    atualizacao tags meta

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -11,9 +11,13 @@ class MyDocument extends Document {
       <Html lang="pt-br">
         <Head> 
         <meta name="description" content="Calcula investimento"/>
-        <meta name="keywords" content="Selic, CDB, LCI, LCA"/>
+        <meta name="keywords" content="selic, cdb, lci, lca, dataselic, data selic, investimentos, investimentos curto prazo, investimentos 2021, samuel claudino, api inflação, api taxa selic, samuel claudino dataselic, calculadora investimentos"/>
         <meta name="author" content="Samuel Claudino"/>
+        <meta name="copyright" content="Samuel Claudino 2021"/>
         <meta name="theme-color" content="#343a40" />
+        <meta http-equiv="content-language" content="pt-br"/>
+        <meta property="og:title" content="DataSelic" />
+        <meta property="og:description" content="Melhores investimentos curto prazo."/>
         <link  href="https://use.fontawesome.com/releases/v5.0.7/css/all.css"/>
         <link rel="shortcut icon" href="/img/logo-dataselic.svg" type="image/x-icon" />
         </Head>
